6eea6fd1ea2441b33ec0b8c6a5eff1eb4196f3a5
santy <santiago.becerra@uptc.edu.co>
2022-05-25 16:51:54 -0500

    add challenge 4

---
+++ challenge4_cicle1.py
@@ -0,0 +1,15 @@
+def ordenes(rutinaContable):
+    print('------------------------ Inicio Registro diario ---------------------------------')
+    listIds = list(map(int,(map(lambda x: x[0], rutinaContable))))
+    ListProds = list(map(lambda x: x[1:], rutinaContable))
+    listF = []
+    count = 0.0
+    for i in range(len(ListProds)):
+        count = 0.0
+        for j in range(len(ListProds[i])):
+            count += ListProds[i][j][1]*ListProds[i][j][2]
+        if(count < 600000): count = count + 25000
+        listF.append(count)
+    for id,total in zip(listIds,listF):
+        print('La factura {} tiene un total en pesos de {:,.2f}'.format(id,total))
+    print('-------------------------- Fin Registro diario ----------------------------------')
